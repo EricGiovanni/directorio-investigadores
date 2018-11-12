489cce1c63a7a5f29b9b189d57ae85cf0eb8ca8c
Rodrigo Salgado Muñoz <30489036+RuySalgado@users.noreply.github.com>
2018-11-11 23:14:42 -0600

    Better example during log in (#6)
    Trying to log in shows an alert instead of redirecting you to a blank page.
    
    The message depends on the provided email, can be either a success message if the email is associated to an user in he data base or a fail message if it is not.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,19 @@
 
 $(document).ready(() => {    
-    $('#logInButton').click(()=>{
-        console.log('test');
+    $('#logInButton').click(function(event){
+        event.preventDefault();
+        var form = $('#logInForm');
+        sendFormData(form);
     });
 });
+
+function sendFormData(form){
+    var url = form.attr("action");
+    var formData = {};
+    $(form).find("input[name]").each(function (index, node) {
+        formData[node.name] = node.value;
+    });
+    $.post(url, formData).done(function (data) {
+        alert(data);
+    });
+}
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,13 +6,17 @@ module.exports = {
     Revisa la base de datos para ver si existe el corre y luego confirma que las contraseñas coincidan.
     */
     confirmLogIn(req,res){
-        var email = req.body.email
-        var password = req.body.password
-        db.sequelize.query('SELECT * FROM User WHERE email=?',{replacements:[email]})
+        var email = req.body.email;
+        var password = req.body.password;
+        console.log(req.body);
+        db.sequelize.query('SELECT * FROM User WHERE email=?',{type: db.Sequelize.QueryTypes.SELECT,raw: true, replacements:[email]})
         .then(user => {
-            console.log(user)
-            res.send(user)
-        })
+            if (user.length!=1) {
+                res.send('Usuario o contraseña inválida.');
+            } else {
+                res.send('Autentificación completada');  
+            }
+        });
     }
 
 
