4db395f8afe737dc1af019cc0df6b1ad89843bc7
Rodrigo Salgado <Ruy@localhost.localdomain>
2018-12-05 19:51:13 -0600

    Added a profile page for the institutions that you can visit using the links in the quick search results

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var db = require('../models/index');
 var userModel = require('../models/user')(db.sequelize,db.Sequelize);
+var institutionModel = require('../models/institution')(db.sequelize,db.Sequelize);
 
 router.get('/usuario/:id/:user',function(req,res,next){
     userModel.findOne({
@@ -14,13 +15,25 @@ router.get('/usuario/:id/:user',function(req,res,next){
             res.redirect('/');
         } else {
             var isOwner = req.isAuthenticated() ? req.params.id == req.user.id : false;
-            res.render('profile',{
+            res.render('user-profile',{
                 isOwner: isOwner,
                 user: user
             })
         }
     })
     
+});
+
+router.get('/institucion/:id/:user', function(req,res,next){
+    institutionModel.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(institution => {
+        res.render('institution-profile',{
+            institution: institution
+        });
+    })
 });
 
 module.exports = router;
