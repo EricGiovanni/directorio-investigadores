3a31431fdb9ef44f168d8ace4cb098e8057532cb
Adrián García Pérez <adrian.garcia04@ciencias.unam.mx>
2018-12-12 00:58:44 -0600

    Sign up validation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,8 +7,45 @@ const sgMail = require('@sendgrid/mail');
 const fs = require('fs');
 const path = require('path');
 const randomstring = require("randomstring");
+const { body, validationResult } = require('express-validator/check');
 
 module.exports = {
+    validate(method) {
+        switch (method) {
+            case 'signUp': {
+                    return [
+                        body('names', 'Los nombres son obligatorios.').not().isEmpty(),
+                        body('last_names', 'Los apellidos son obligatorios.').not().isEmpty(),
+                        body('email', 'La dirección de correo electrónico es obligatoria.').not().isEmpty(),
+                        body('email', 'Dirección de correo electrónico inválida.').isEmail(),
+                        body('password', 'La contraseña es obligatoria.').not().isEmpty(),
+                        body('passwordMatch', 'La confirmación de contraseña es '
+                            + 'obligatoria.').not().isEmpty(),
+                        body('accountType', 'El tipo de cuenta solo puede ser '
+                            + '"estudiante" o "investigador".')
+                            .isIn(['student', 'researcher']),
+                        body('email').custom((value) => {
+                            return User.findOne({
+                                where: {
+                                    email: value,
+                                }
+                            })
+                            .then((existingUser) => {
+                                if (existingUser)
+                                    return Promise.reject('El correo electrónico '
+                                        + 'ya ha sido registrado');
+                            });
+                        }),
+                        body('password').custom((value, { req }) => {
+                            if (value != req.body.passwordMatch)
+                                return Promise.reject('Las contraseñas no coinciden');
+                            return Promise.resolve();
+                        }),
+                    ]
+                }
+                break;
+        }
+    },
     logInIndex(req, res) {
         if (req.isAuthenticated())
             res.redirect('/')
@@ -44,25 +81,14 @@ module.exports = {
         else
             res.render('signup');
     },
-    signUp(req, res) {
-        User.findOne({
-            where: {
-                email: req.body.email,
-            }
-        })
-        .then((existingUser) => {
-            if (existingUser) {
-                return res.render('signup', {
-                    error: 'El correo electrónico ya ha sido registrado'
-                });
-            }
-            else if (req.body.password != req.body.passwordMatch) {
-                return res.render('signup', {
-                    error: 'Las contraseñas no coinciden'
-                });
-            }
-        })
-        .then(() => {
+    signUp(req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render('signup', {
+                errors: errors.array()
+            });
+        }
+        else {
             User.create({
                 names: req.body.names,
                 last_names: req.body.last_names,
@@ -74,8 +100,8 @@ module.exports = {
             })
             .then((user) => {
                 let confirmation_link = "http://" + process.env.HOST + ":"
-                                        + process.env.PORT + "/"
-                                        + "confirmacion/" + user.token;
+                + process.env.PORT + "/"
+                + "confirmacion/" + user.token;
                 var emailHTML = path.join(__dirname, "..", "..", "views/email/", "confirm.html");
                 emailHTML = fs.readFileSync(emailHTML).toString();
                 emailHTML = emailHTML.replace(/{{user.names}}/gm, user.names);
@@ -94,7 +120,7 @@ module.exports = {
                             user_id: user.id,
                             createdAt: new Date(),
                             updatedAt: new Date(),
-                        })
+                        });
                     }
                     else if (req.body.accountType == "researcher") {
                         Researcher.create({
@@ -107,7 +133,7 @@ module.exports = {
                 });
             })
             .catch(error => res.status(400).send(error));
-        });
+        }
     },
     logOut(req, res){
 
